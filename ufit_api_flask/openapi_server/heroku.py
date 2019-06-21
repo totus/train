4c4697af98b80f1ef3fd5d9c3e6214954e24d24f
Eugene Kondrashev <eugene.kondrashev@netrounds.com>
2019-06-21 17:13:59 +0300

    All routes route

--- ufit_api_flask/openapi_server/heroku.py
+++ ufit_api_flask/openapi_server/heroku.py
@@ -3,6 +3,7 @@
 import connexion
 
 from openapi_server import encoder
+from flask import Flask, render_template, url_for
 
 
 app = connexion.App(__name__, specification_dir='./openapi/')
@@ -10,6 +11,24 @@ app.app.json_encoder = encoder.JSONEncoder
 app.add_api('openapi.yaml',
             arguments={'title': 'Fitness Coaching Marketplace'},
             pythonic_params=True)
+
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+
+@app.route("/site-map")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+    # Filter out rules we can't navigate to in a browser
+    # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    return render_template("all_links.html", links=links)
+
 def main():
     app.run(port=8080)
 
